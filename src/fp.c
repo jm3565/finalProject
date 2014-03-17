703d168a3f853ac319e169272f9606aa463481d1
jm3565 <jnmunoz74@gmail.com>
2014-03-17 00:28:04 -0400

    added code for displaying on window

--- src/fp.c
+++ src/fp.c
@@ -18,19 +18,20 @@ char* blur(char* imgName) {
 
 char* downloadImage(char* webPath){
 	pid_t pid;
-	char *const paramList[] = {"/usr/bin/wget", webPath, "-P", "./images", NULL};
+        char * fileName;
+        char *const paramList[] = {"/usr/bin/wget", webPath, "-P", IMGFOLDER, NULL};
 
-	if((pid = fork()) == 0){
-		setpgid(0,0);
-		if(execv("/usr/bin/wget", paramList) < 0){
-			printf("%s: Command not found.\n", "/usr/bin/wget");
-			exit(0);
-		}
-	}
-	wait(NULL);
-	char *fileName = basename(webPath);
-	printf("Image Downloaded\n");
-	return fileName;
+        if((pid = fork()) == 0){
+                setpgid(0,0);
+                if(execv("/usr/bin/wget", paramList) < 0){
+                        printf("%s: Command not found.\n", "/usr/bin/wget");
+                        exit(0);
+                }
+        }
+        wait(NULL);
+        fileName = basename(webPath);
+        printf("Image Downloaded\n");
+        return fileName;
 }
 
 IplImage* loadImage(char* imgName){
@@ -46,6 +47,75 @@ IplImage* loadImage(char* imgName){
 
 	return rv;
 }
+
+int sendImage(char * imgEdited, int conSocket){
+//char* sendImage(char * imgEdited){
+        char * res;
+        char * fname;
+        long len, ret;
+        FILE * ifp;
+        char * rv;
+
+        struct stat stat_buf;
+        off_t offset = 0;
+        int rc;
+
+        fname = malloc(sizeof(char) * 128);
+
+        strcat(fname, IMGFOLDER); /* build the path name of the file */
+        strcat(fname, "/");
+        strcat(fname, imgEdited);
+
+        /*
+        fd = open(fname, O_RDONLY);
+        if(fd == -1){
+                fprintf(stderr, "unable to open '%s'", fname);
+                exit(1);
+        }
+        fstat(fd, &stat_buf);
+
+        offset = 0;
+        */
+
+        /*
+        ifp = fopen(fname, "r+b");
+        if(ifp == NULL){
+                printf("%s: File not found.\n", fname);
+                exit(0);
+        }
+        
+
+        printf("file opened: %s", fname);
+
+        fseek(ifp, 0, SEEK_END);
+        len = ftell(ifp);
+        fseek(ifp, 0, SEEK_SET);
+
+        //len = (long)fseek(ifp, (off_t)0, SEEK_END); // lseek to the file end to find the length //
+        printf("image length: %ld\n", len);     
+        //(void)fseek(ifp, (off_t)0, SEEK_SET); // lseek back to the file start ready for reading //
+        */
+
+        rv = malloc(BUFSIZE * sizeof(char));// TODO: allocate a big chunk of memory for the result. Like 2048 characters
+        sprintf(rv, "HTTP/1.1 200 OK\n");
+
+        res = &(rv[strlen("HTTP/1.1 200 OK\n")]);
+        sprintf(res, "Content-Length: %ld\nContent-Type: %s\n\n<html>\n<body>\n<img src=\"%s\">\n</body>\n</html>", len, "image/jpg", fname);
+
+        printf("%s\n", rv);
+
+        /*
+        (void)write(conSocket, rv, strlen(res)); // send image data throught the socket //
+
+        while((ret = read(ifp, rv, BUFSIZE)) > 0){
+                (void)write(conSocket, rv, ret);
+        }
+        sleep(1); // allow socket to drain before signalling the socket is closed //
+        printf("image Sent\n");
+        */
+        //return rv;
+}
+
 
 /**
  * This function is the big one of this assignment.
@@ -70,7 +140,11 @@ char* handle_request(char * req) {
 	               // for example, if you go to http://localhost:8000/?f=fib&n=10, this will be "f=fib"
 	char * atoken; // A token representing the argument to the function, i.e., "n=10"
 	char * tmp; // used to point to where the arguments start
+	
+	char * imgDownloaded;
+    char * imgEdited;
 
+/*
 	rv = malloc(4096 * sizeof(char));// TODO: allocate a big chunk of memory for the result. Like 2048 characters
 
 	// TODO: write the first line of the HTML header to rv.
@@ -80,7 +154,7 @@ char* handle_request(char * req) {
 
 	// FREEBEE: set the 'res' pointer to be in the location after the header
 	res = &(rv[strlen("HTTP/1.1 200 OK\n\n")]);
-
+*/	
     // Use strncmp, see http://www.cplusplus.com/reference/cstring/strncmp/
 	if(strncmp(req, "GET", 3) == 0) // TODO: check if the request is a GET request.
     {
@@ -103,21 +177,27 @@ char* handle_request(char * req) {
 	// TODO: see if 'ftoken' is any of our functions
 	if(strcmp(ftoken, "f=blur") == 0){
 		//print("%s\n", downloadImage(argv[1]));
-		sprintf(res, "%s", downloadImage(atoken));   // TODO: if ftoken is "f=fib", call fib and print results to res
-		
-		//sprintf(res, "%s", atoken);   // TODO: if ftoken is "f=fib", call fib and print results to res
+                //sprintf(res, "%s", downloadImage(atoken));   // TODO: if ftoken is "f=fib", call fib and print results to res
+
+                //sprintf(res, "%s", atoken);   // TODO: if ftoken is "f=fib", call fib and print results to res
+
+                //1)download the image (sets fileName of downloaded image)
+                        // param: the webPath of the image
+                        // returns 0 on success, -1 on error
+                //imgDownloaded = downloadImage(atoken);
+                imgEdited = downloadImage(atoken);
+                //2)call function requested (sets the name of the imaged edited)
+                        // param: the name of the image downloaded
+                        // returns 0 on success, -1 on error
+                //imgEdited = blur(imgDownloaded);
+                //printf("imgE: %s\n", imgEdited);
+                //3)open the image changed and send the HTTP response
+                        // param: the name of the image edited
+                        // param: the partial HTTP response to be filled
+                        // return 0 on success, -1 on error
+                //sendImage(imgEdited, con_Socket);
+                //rv = sendImage(imgEdited);
         }
-	/*
-        if(strcmp(ftoken, "f=numFiles") == 0){
-            sprintf(res, "%d", numFiles(atoken));  // TODO: if numFiles, call function and print result
-        }
-        if(strcmp(ftoken, "f=getFileSize") == 0){
-            sprintf(res, "%ld", getFileSize(atoken));  // TODO: if getFileSize, call function and print result
-        }
-        if(strcmp(ftoken, "f=getEnvValue") == 0){
-            sprintf(res, "%s", getEnvValue(atoken)); // TODO: if getEnvValue, call function and print result
-        }
-	*/
     }
-	return rv; // TODO: return result
+	return imgEdited; // TODO: return result
 }
